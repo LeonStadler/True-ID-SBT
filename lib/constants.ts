3dff8db4d35f5cf25f37272cc89078fe85027197
Nino <162924832+Le0Nin0@users.noreply.github.com>
2024-04-16 15:23:24 +0200

    New contract id for amoy testnet

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,2 @@
-export const contractAddress = "0xC99B275dF2DDAad1CbB277eB76aD9EE6252B7899";
+//export const contractAddress = "0xC99B275dF2DDAad1CbB277eB76aD9EE6252B7899"; old contract
+export const contractAddress = "0x08ac3828790818a58675ab67852c52ea06ab423b";
