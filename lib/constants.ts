f3a4fbe355ee45ee8f777b7a27148589d1e11003
Nino <162924832+Le0Nin0@users.noreply.github.com>
2024-04-24 17:18:46 +0200

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,2 @@
 //export const contractAddress = "0xC99B275dF2DDAad1CbB277eB76aD9EE6252B7899"; old contract
-export const contractAddress = "0x08ac3828790818a58675ab67852c52ea06ab423b";
+export const contractAddress = "0xAAC8A21a7cb1c4b23680fA5eD90D2B8De4A7AC40";
