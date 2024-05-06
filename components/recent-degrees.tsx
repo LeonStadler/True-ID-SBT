2c3c443150634592f3afedc1a8b1f822528784a9
Nino <162924832+Le0Nin0@users.noreply.github.com>
2024-05-06 15:35:54 +0200

    Update recent-degrees.tsx

--- components/recent-degrees.tsx
+++ components/recent-degrees.tsx
@@ -71,6 +71,8 @@ export default function RecentClaimedDegreesComponent() {
         <Table>
           <TableCaption>
             The following list contains the idenitifiers of users who have minted a TrueID University Degree Token after earning a degree.
+          </TableCaption>
+          <TableCaption>
             Please note that this is a prototype and that no information is provided about the legitimacy of the data.
           </TableCaption>
           <TableHeader>
